d9b3bcf2134b441f07efc5a98bea3922124d2d1a
Tux2 <thetux2@gmail.com>
2026-01-26 20:20:14 -0700

    Markdown Parsed

--- src/markdownfunctions.py
+++ src/markdownfunctions.py
@@ -1,4 +1,5 @@
 from textnode import *
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -25,3 +26,65 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             plain_txt = TextNode(subtext, TextType.PLAIN)
             new_nodes.append(plain_txt)
     return new_nodes
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\]]+)\]\((http[s]?:\/\/[a-zA-Z0-9\.\/]+)\)", text)
+    return matches
+
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\]]+)\]\((http[s]?:\/\/[a-zA-Z0-9\.\/]+)\)",  text)
+    return matches
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.PLAIN:
+            new_nodes.append(old_node)
+            continue
+        subtext = old_node.text
+        images = extract_markdown_images(old_node.text)
+        for image in images:
+            split_1 = subtext.split(f"![{image[0]}]({image[1]})", maxsplit=1)
+            if split_1[0] != "":
+                plain_txt = TextNode(split_1[0], TextType.PLAIN)
+                new_nodes.append(plain_txt)
+            link_txt = TextNode(image[0], TextType.IMAGE, image[1])
+            new_nodes.append(link_txt)
+            if len(split_1) > 1:
+                subtext = split_1[len(split_1) - 1]
+            else:
+                subtext = ""
+        if subtext != "":
+            plain_txt = TextNode(subtext, TextType.PLAIN)
+            new_nodes.append(plain_txt)
+    return new_nodes
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.PLAIN:
+            new_nodes.append(old_node)
+            continue
+        subtext = old_node.text
+        images = extract_markdown_links(old_node.text)
+        for image in images:
+            split_1 = subtext.split(f"[{image[0]}]({image[1]})", maxsplit=1)
+            if split_1[0] != "":
+                plain_txt = TextNode(split_1[0], TextType.PLAIN)
+                new_nodes.append(plain_txt)
+            link_txt = TextNode(image[0], TextType.LINK, image[1])
+            new_nodes.append(link_txt)
+            if len(split_1) > 1:
+                subtext = split_1[len(split_1) - 1]
+            else:
+                subtext = ""
+        if subtext != "":
+            plain_txt = TextNode(subtext, TextType.PLAIN)
+            new_nodes.append(plain_txt)
+    return new_nodes
+
+def text_to_textnodes(text):
+    text_node = TextNode(text, TextType.PLAIN)
+    result_nodes = split_nodes_delimiter(split_nodes_delimiter(split_nodes_delimiter([text_node], "**", TextType.BOLD), "_", TextType.ITALIC), "`", TextType.CODE)
+    linked_nodes = split_nodes_link(split_nodes_image(result_nodes))
+    return linked_nodes
--- src/test_markdownfunctions.py
+++ src/test_markdownfunctions.py
@@ -47,3 +47,100 @@ class TestMarkdownParse(unittest.TestCase):
         node1 = TextNode("This is a text node with **some bolded text", TextType.PLAIN)
         with self.assertRaises(Exception):
             split_nodes_delimiter([node1], "**", TextType.BOLD)
+    
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)")
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+    
+    def test_extract_markdown_images_with_crazy_description(self):
+        matches = extract_markdown_images("This is text with an ![Image, but with some interesting punctionation, that'll knock your socks off!](https://i.imgur.com/zjjcJKZ.png)")
+        self.assertListEqual([("Image, but with some interesting punctionation, that'll knock your socks off!", "https://i.imgur.com/zjjcJKZ.png")], matches)
+    
+    def test_extract_more_markdown_images(self):
+        matches = extract_markdown_images("This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)")
+        self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://i.imgur.com/zjjcJKZ.png)")
+        self.assertListEqual([("link", "https://i.imgur.com/zjjcJKZ.png")], matches)
+    
+    def test_extract_markdown_links_with_crazy_description(self):
+        matches = extract_markdown_links("This is text with a [Link, but with some interesting punctionation, that'll knock your socks off!](https://i.imgur.com/zjjcJKZ.png)")
+        self.assertListEqual([("Link, but with some interesting punctionation, that'll knock your socks off!", "https://i.imgur.com/zjjcJKZ.png")], matches)
+    
+    def test_extract_more_markdown_links(self):
+        matches = extract_markdown_links("This is text with a [rick roll](https://i.imgur.com/aKaOqIh.gif) and [obi wan](https://i.imgur.com/fJRm4Vk.jpeg)")
+        self.assertListEqual([("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")], matches)
+    
+    def test_split_images(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            TextType.PLAIN,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.PLAIN),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.PLAIN),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with an [link](https://i.imgur.com/zjjcJKZ.png) and another [second link](https://i.imgur.com/3elNhQu.png)",
+            TextType.PLAIN,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.PLAIN),
+                TextNode("link", TextType.LINK, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.PLAIN),
+                TextNode(
+                    "second link", TextType.LINK, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+    
+    def test_split_images_and_links(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another [second link](https://i.imgur.com/3elNhQu.png)",
+            TextType.PLAIN,
+        )
+        new_nodes = split_nodes_link(split_nodes_image([node]))
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.PLAIN),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.PLAIN),
+                TextNode(
+                    "second link", TextType.LINK, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+    
+    def test_combined(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        result = text_to_textnodes(text)
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.PLAIN),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.PLAIN),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.PLAIN),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.PLAIN),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.PLAIN),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            result,
+        )
